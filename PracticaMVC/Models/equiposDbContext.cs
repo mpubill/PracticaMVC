f4afa0e539359acc311d1dd1cfe0d604a56385c3
melan <melan@LAPTOP-30ROFJGU>
2023-04-15 10:16:37 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Models/equiposDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext : DbContext
+    {
+        public equiposDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<marcas> marcas { get; set; }
+    }
+}
